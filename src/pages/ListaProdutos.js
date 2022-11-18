e107a0a797ef15fe511e9470223d7fe83324f6f9
matheus.bonnet.glo <matheus.bonnet@globalhitss.com.br>
2022-11-18 14:03:42 -0300

    Gerando Relatorio para o Estoque

--- src/pages/ListaProdutos.js
+++ src/pages/ListaProdutos.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect, history, Link } from 'react';
 import Header from '../components/Header';
 import { useNavigate } from 'react-router-dom';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+import { BsFileEarmarkPdfFill } from 'react-icons/bs'
 
 //Services
 import api from '../services/api';
@@ -48,8 +51,9 @@ function ListaProdutos() {
             headers: {
                 Authorization: `Bearer ${accessToken}`
             }
-            
+
         });
+        console.log(response.data.data)
         setProducts(response.data.data)
     }
 
@@ -57,9 +61,82 @@ function ListaProdutos() {
         fetchMoreProducts();
     }, [])
 
+    function geraPdf() {
+
+        pdfMake.vfs = pdfFonts.pdfMake.vfs;
+
+        const reportTitle = [
+            {
+                text: 'Estoque',
+                fontSize: 15,
+                bold: true,
+                margin: [15, 20, 0, 45]
+            }
+        ];
+
+        const dados = products.map((produto) => {
+            return [
+                { text: produto.nome, fontSize: 9, margin: [0, 2, 0, 2] },
+                { text: produto.ativo, fontSize: 9, margin: [0, 2, 0, 2] },
+                { text: produto.categoria, fontSize: 9, margin: [0, 2, 0, 2] },
+                { text: produto.fornecedor, fontSize: 9, margin: [0, 2, 0, 2] },
+                { text: produto.descricao, fontSize: 9, margin: [0, 2, 0, 2] },
+                { text: produto.quantidade, fontSize: 9, margin: [0, 2, 0, 2] }
+            ]
+        });
+
+        const details = [
+            {
+                table: {
+                    headerRows: 1,
+                    widths: ['*', '*', '*', '*', '*', '*'],
+                    body: [
+                        [
+                            { text: 'Nome', style: 'tableHeader', fontSize: 10 },
+                            { text: 'Ativo', style: 'tableHeader', fontSize: 10 },
+                            { text: 'Categoria', style: 'tableHeader', fontSize: 10 },
+                            { text: 'Fornecedor', style: 'tableHeader', fontSize: 10 },
+                            { text: 'Descrição', style: 'tableHeader', fontSize: 10 },
+                            { text: 'Quantidade', style: 'tableHeader', fontSize: 10 }
+                        ],
+                        ...dados
+                    ]
+                },
+                layout: 'lightHorizontalLines'
+            }
+        ];
+
+
+
+        const docDefinitons = {
+            pageSize: 'A4',
+            pageMargins: [15, 50, 15, 40],
+            header: [reportTitle],
+            content: [details],
+            footer: rodape
+        }
+
+        pdfMake.createPdf(docDefinitons).download();
+
+
+
+    }
+
+    function rodape(currentPage, pageCount) {
+        return [
+            {
+                text: currentPage + ' / ' + pageCount,
+                aligment: 'right',
+                fontSize: 9,
+                margin: [0, 10, 20, 0]
+            }
+        ]
+    };
+
+
     return (
         <div className="book-container">
-            <Header/>
+            <Header />
 
             <h1 className='titulo-lista'>Produtos</h1>
             <ul className='box-lista'>
@@ -74,17 +151,22 @@ function ListaProdutos() {
                         <p className='descricao-campo-lista'>{product.descricao}</p>
                         <strong className='titulo-campo-lista'>QUANTIDADE:</strong>
                         <p className='valor-campo-lista'>{product.quantidade}</p>
-                        
+
                         <input className='btn-lista-edit' onClick={() => editProduct(product.id)}
-                         type="submit" value=""/>
-                          
-                    
+                            type="submit" value="" />
+
+
                         <input className='btn-lista-delete' onClick={() => deleteProduct(product.id)}
-                         type="button" value=""/>
+                            type="button" value="" />
 
                     </li>
                 ))}
             </ul>
+
+            <h3> Gerar Pdf:</h3>
+            <button id="pdf" type="button" onClick={geraPdf}>
+                <BsFileEarmarkPdfFill />
+            </button>
         </div>
     );
 
